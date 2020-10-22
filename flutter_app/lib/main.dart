3895777c7273740b0070cc8255a270bb6275ea9a
Ruan Tiengo <ruantiengo@gmail.com>
2020-10-22 14:21:41 -0300

    first commit

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(new MaterialApp(
+    title: "Aniversario 1 month",
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+
+      ]
+    )
+  ));
+}
