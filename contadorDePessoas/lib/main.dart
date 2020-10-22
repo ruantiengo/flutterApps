b89cf9edeab625e74f7eb33d1d007c1ec5b5ee6e
Ruan Tiengo <ruantiengo@gmail.com>
2020-10-22 16:03:06 -0300

    Contador de pessoas implementado

--- contadorDePessoas/lib/main.dart
+++ contadorDePessoas/lib/main.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(new MaterialApp(
-    title: "Aniversario 1 month",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
 
-      ]
-    )
+void main() {
+  runApp(MaterialApp(
+    title: 'Contador de Pessoas',
+    home: Home(),
   ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+
+  void _changePeople(int x) {
+    setState(() {
+      _people += x;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          'images/restaurant.jpg',
+          fit: BoxFit.cover,
+          height: 1000,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Pessoas : $_people',
+              style: TextStyle(color: Colors.blue, fontSize: 40),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: FlatButton(
+                      onPressed: () {
+                        _changePeople(1);
+                      },
+                      child: Text(
+                        '+1',
+                        style: TextStyle(color: Colors.blue, fontSize: 40),
+                      )),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: FlatButton(
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                    child: Text('-1',
+                        style: TextStyle(color: Colors.blue, fontSize: 40)),
+                  ),
+                )
+              ],
+            ),
+            Text(
+              'Pode entrar',
+              style: TextStyle(fontSize: 30, color: Colors.blue),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 }
